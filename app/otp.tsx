da0c9d477ff15b935bce44c160adcc1de313fa88
Evan <evanz1215@gmail.com>
2024-06-23 23:11:14 +0800

    feat: Update OTP screen with WhatsApp verification message
    The code changes in this commit update the OTP screen in the `otp.tsx` file. The screen now displays a WhatsApp verification message with the description "WhatsApp will need to verify your account. Carrier charges may apply." The styling of the screen has also been updated to use the `Colors.background` color for the container background and the `Colors.gray` color for the description text.

--- app/otp.tsx
+++ app/otp.tsx
@@ -1,11 +1,51 @@
-import { View, Text } from "react-native";
+import Colors from "@/constants/Colors";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  Linking,
+  StyleSheet,
+  View,
+} from "react-native";
 
 const Page = () => {
+  const [loading, setLoading] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const router = useRouter();
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 90 : 0;
+
+  const openLink = () => {
+    Linking.openURL("https://google.com");
+  };
+
+  const sendOTP = async () => {};
+
+  const trySignIn = async () => {};
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          WhatsApp will need to verify your account. Carrier charges may apply.
+        </Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: Colors.background,
+    gap: 20,
+  },
+  description: {
+    fontSize: 14,
+    color: Colors.gray,
+  },
+});
 export default Page;
